c880a2533c0ffa5bf50d0d2dad0d6991011b3a86
Dax <limeside@gmail.com>
2016-10-17 17:14:27 -0500

    Add dependencies, begin to refactor. :rabbit:

---
+++ app/app.jsx
@@ -0,0 +1,12 @@
+'use strict'
+
+var React = require('react'),
+    ReactDOM = require('react-dom');
+
+import LoginForm from 'LoginForm'
+
+
+ReactDOM.render(
+  <h1>Connection Test</h1>,
+  document.getElementById('app')
+);
