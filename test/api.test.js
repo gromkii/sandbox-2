e8829696757260cf901be95af66ebc530fa35303
Dax <limeside@gmail.com>
2017-01-22 22:00:49 -0600

    Add edit user route test, reorganized API tests. :rabbit:

--- test/api.test.js
+++ test/api.test.js
@@ -32,58 +32,8 @@ describe('API', () => {
       })
   })
 
-  it('should return seeded users.', done => {
-    // According to the seed file, there should be 3 users.
-    request
-      .get('/api/users')
-      .expect(200)
-      .end((err, res) => {
-        let users = res.body;
-        expect(users.length).to.eq(3);
-        done()
-      })
-  })
 
-  it('should return a specific user', done => {
-    request
-      .get('/api/users/1')
-      .expect(200)
-      .end((err, res) => {
-        let user = res.body;
-        expect(user).to.have.property('username')
-        expect(user).to.have.property('full_name')
-        expect(user).to.have.property('profile_url')
-        expect(user).to.have.property('about_me');
-        done();
-      })
-  })
-
-  it('should post a new user', done => {
-    let data = {
-      username:'Test',
-      email:'testing@test.com',
-      password:'test',
-      about_me:'Im testing.',
-      profile_url:'http://placecage.com/200/200',
-      full_name:'Jessica Cage'
-    }
-
-    request
-      .post('/api/users')
-      .send(data)
-      .expect(200)
-      .end((err, res) => {
-        request
-          .get('/api/users')
-          .expect(200)
-          .end((err, res) => {
-            let users = res.body;
-            expect(users.length).to.eq(4);
-            done();
-          })
-      });
-  });
-  describe('Login', () => {
+  describe('Auth Routes', () => {
     it('should login with correct credentials', done => {
       let data = {
         username:'met2002',
@@ -141,6 +91,77 @@ describe('API', () => {
               expect(data.error).to.eq('Something went wrong.');
               done();
             })
+        });
+    });
+  });
+
+  describe('User Routes', () => {
+    it('should return seeded users.', done => {
+      // According to the seed file, there should be 3 users.
+      request
+        .get('/api/users')
+        .expect(200)
+        .end((err, res) => {
+          let users = res.body;
+          expect(users.length).to.eq(3);
+          done()
+        })
+    })
+
+    it('should return a specific user', done => {
+      request
+        .get('/api/users/1')
+        .expect(200)
+        .end((err, res) => {
+          let user = res.body;
+          expect(user).to.have.property('username')
+          expect(user).to.have.property('full_name')
+          expect(user).to.have.property('profile_url')
+          expect(user).to.have.property('about_me');
+          done();
+        })
+    })
+
+    it('should post a new user', done => {
+      let data = {
+        username:'Test',
+        email:'testing@test.com',
+        password:'test',
+        about_me:'Im testing.',
+        profile_url:'http://placecage.com/200/200',
+        full_name:'Jessica Cage'
+      }
+
+      request
+        .post('/api/users')
+        .send(data)
+        .expect(200)
+        .end((err, res) => {
+          request
+            .get('/api/users')
+            .expect(200)
+            .end((err, res) => {
+              let users = res.body;
+              expect(users.length).to.eq(4);
+              done();
+            })
+        });
+    });
+
+    it('should update user information', done => {
+      let data = {
+        full_name:'Nick Cage'
+      };
+
+      request
+        .put('/api/users/1')
+        .send(data)
+        .expect(200)
+        .end((err, res) => {
+          let user = res.body.user;
+
+          expect(user.full_name).to.eq('Nick Cage');
+          done();
         });
     });
   });
